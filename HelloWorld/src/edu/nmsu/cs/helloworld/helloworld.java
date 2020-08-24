334c6f53e9ef9fe0d323e1f755131993eed3c024
Devin <devintha@nmsu.edu>
2020-08-23 22:40:03 -0600

    Added HelloWorld project folder with helloworld.java

---
+++ HelloWorld/src/edu/nmsu/cs/helloworld/helloworld.java
@@ -0,0 +1,11 @@
+package edu.nmsu.cs.helloworld;
+
+public class helloworld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World. ");
+
+    }//end of main
+
+}//end of helloworld
